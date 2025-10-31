d838e8f5fb09db4a440992434ecfd91414ac396e
simeonov90 <vilensimeonov@gmail.com>
2025-10-31 22:50:02 +0200

    add Author and Book domain models

---
+++ src/LibraryManagement.Domain/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagement.Domain.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime? DateOfBirth { get; set; }
+        public string? Country { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ src/LibraryManagement.Domain/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagement.Domain.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int? Year { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public int AuthorId { get; set; }
+        public Author Author { get; set; } = null!;
+    }
+}
