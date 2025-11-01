82dafde03f94fc58a1d7d199c342f250258c4e78
simeonov90 <vilensimeonov@gmail.com>
2025-11-01 08:07:34 +0200

    add base mapping

---
+++ src/LibraryManagement.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using LibraryManagement.Application.Services.Authors.Dtos;
+using LibraryManagement.Application.Services.Books.Dtos;
+using LibraryManagement.Domain.Models;
+
+namespace LibraryManagement.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Author, AuthorDto>();
+            CreateMap<CreateAuthorDto, Author>();
+
+            CreateMap<Book, BookDto>();
+            CreateMap<CreateBookDto, Book>();
+        }
+    }
+}
