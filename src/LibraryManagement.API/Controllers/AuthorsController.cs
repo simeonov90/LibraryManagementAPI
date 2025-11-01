ee52c087734b3218bc15dc14d4fb080e7d7c5d8f
simeonov90 <vilensimeonov@gmail.com>
2025-11-02 01:17:09 +0200

    add controllers with base endpoints

---
+++ src/LibraryManagement.API/Controllers/AuthorsController.cs
@@ -0,0 +1,97 @@
+﻿using LibraryManagement.API.Extensions;
+using LibraryManagement.Application.Services.Authors;
+using LibraryManagement.Application.Services.Authors.Dtos;
+using LibraryManagement.Domain.Exceptions;
+using LibraryManagement.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly AuthorService _authorService;
+
+        public AuthorsController(AuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AuthorDto>>> GetAuthors()
+        {
+            var authors = await _authorService.GetAllAsync();
+
+            return Ok(authors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuthorDto>> GetAuthor(int id)
+        {
+            var author = await _authorService.GetByIdAsync(id);
+
+            if (author == null)
+            {
+                throw new NotFoundException(nameof(Author), id);
+            }
+
+            return Ok(author);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuthorDto>> CreateAuthor([FromBody] CreateAuthorDto createAuthorDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.GetErrors();
+                throw new ValidationException(errors);
+            }
+
+            var author = await _authorService.CreateAsync(createAuthorDto);
+
+            return CreatedAtAction(nameof(GetAuthor), new { id = author.Id }, author);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAuthor(int id, [FromBody] UpdateAuthorDto updateAuthorDto)
+        {
+            if (id != updateAuthorDto.Id)
+            {
+                throw new BadRequestException("Author ID mismatch.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.GetErrors();
+                throw new ValidationException(errors);
+            }
+
+            try
+            {
+                await _authorService.UpdateAsync(updateAuthorDto);
+            }
+            catch (Exception ex)
+            {
+                throw new NotFoundException(ex.Message);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(int id)
+        {
+            try
+            {
+                await _authorService.DeleteAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw new NotFoundException(ex.Message);
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ src/LibraryManagement.API/Controllers/BooksController.cs
@@ -0,0 +1,97 @@
+﻿using LibraryManagement.API.Extensions;
+using LibraryManagement.Application.Services.Books;
+using LibraryManagement.Application.Services.Books.Dtos;
+using LibraryManagement.Domain.Exceptions;
+using LibraryManagement.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookServices _bookServices;
+
+        public BooksController(BookServices bookServices)
+        {
+            _bookServices = bookServices;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookDto>>> GetBooks()
+        {
+            var books = await _bookServices.GetAllAsync();
+
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookDto>> GetBook(int id)
+        {
+            var book = await _bookServices.GetByIdAsync(id);
+
+            if (book == null)
+            {
+                throw new NotFoundException(nameof(Book), id);
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BookDto>> CreateBook([FromBody] CreateBookDto createBookDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.GetErrors();
+                throw new ValidationException(errors);
+            }
+
+            var book = await _bookServices.CreateAsync(createBookDto);
+
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, [FromBody] UpdateBookDto updateBookDto)
+        {
+            if (id != updateBookDto.Id)
+            {
+                throw new BadRequestException("Book ID mismatch.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.GetErrors();
+                throw new ValidationException(errors);
+            }
+
+            try
+            {
+                await _bookServices.UpdateAsync(updateBookDto);
+            }
+            catch (Exception ex)
+            {
+                throw new NotFoundException(ex.Message);
+            }
+            
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            try
+            {
+                await _bookServices.DeleteAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw new NotFoundException(ex.Message);
+            }
+
+            return NoContent();
+        }
+    }
+}
